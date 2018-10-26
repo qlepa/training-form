12bbcc1baf828977b969c52af042e11200e6dd28
Tomasz Klepacki <t.klepacki@gmail.com>
2018-10-26 13:58:16 +0200

    Add gitpages config

---
+++ docs/precache-manifest.2e3cf42b7f603dfc0559534d24e4ea20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce2f292cbae5a4402ed4e33d8ed47d84",
+    "url": "./static/media/pitch.ce2f292c.png"
+  },
+  {
+    "revision": "c5c4668f532199a2883749690dd3ca5b",
+    "url": "./static/media/passShot.c5c4668f.png"
+  },
+  {
+    "revision": "53ee6a7a17284acfdb8fa0aec9fa5779",
+    "url": "./static/media/map.53ee6a7a.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "760c72481096b5b31746",
+    "url": "./static/js/main.760c7248.chunk.js"
+  },
+  {
+    "revision": "23533d1596597722093d",
+    "url": "./static/js/1.23533d15.chunk.js"
+  },
+  {
+    "revision": "760c72481096b5b31746",
+    "url": "./static/css/main.23c20a74.chunk.css"
+  },
+  {
+    "revision": "5545eda482fb3f75b68c7dbea19f7095",
+    "url": "./index.html"
+  }
+];
